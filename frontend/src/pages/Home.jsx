373a95ed5985f1799246a447c2f87ceafeec9ba4
DksSritamJagabandhan <dkssritamjagabandhan@gmail.com>
2026-08-16 02:44:46 +0530

    NONE

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,13 +3,12 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className="homepage">
-      <h1>Welcome to <span>SplitKaro</span></h1>
+      <h1>Welcome to <span>SplitIt</span></h1>
       <p>Split expenses. Manage groups. Keep track of everything.</p>
 
       <Link to="/login">
         <button>Login</button>
       </Link>
-      <br></br><br></br>
       <Link to="/register">
         <button>Register</button>
       </Link>
